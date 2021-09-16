27d25f97ad74a4e12f2c0e6073995621e7e04dbc
JameJohn136 <jamejohn136@outlook.com>
2021-09-16 14:15:42 -0400

    Add project files.

---
+++ VariablesPractice/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace VariablesPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayLabel = new System.Windows.Forms.Label();
+            this.wayneButton = new System.Windows.Forms.Button();
+            this.areaButton = new System.Windows.Forms.Button();
+            this.carpetButton = new System.Windows.Forms.Button();
+            this.saleButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // displayLabel
+            // 
+            this.displayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayLabel.Location = new System.Drawing.Point(231, 13);
+            this.displayLabel.Name = "displayLabel";
+            this.displayLabel.Size = new System.Drawing.Size(530, 428);
+            this.displayLabel.TabIndex = 0;
+            this.displayLabel.Text = "...";
+            this.displayLabel.Click += new System.EventHandler(this.displayLabel_Click);
+            // 
+            // wayneButton
+            // 
+            this.wayneButton.Location = new System.Drawing.Point(70, 91);
+            this.wayneButton.Name = "wayneButton";
+            this.wayneButton.Size = new System.Drawing.Size(75, 23);
+            this.wayneButton.TabIndex = 1;
+            this.wayneButton.Text = "Hockey";
+            this.wayneButton.UseVisualStyleBackColor = true;
+            this.wayneButton.Click += new System.EventHandler(this.wayneButton_Click);
+            // 
+            // areaButton
+            // 
+            this.areaButton.Location = new System.Drawing.Point(70, 185);
+            this.areaButton.Name = "areaButton";
+            this.areaButton.Size = new System.Drawing.Size(75, 23);
+            this.areaButton.TabIndex = 2;
+            this.areaButton.Text = "Area";
+            this.areaButton.UseVisualStyleBackColor = true;
+            this.areaButton.Click += new System.EventHandler(this.areaButton_Click);
+            // 
+            // carpetButton
+            // 
+            this.carpetButton.Location = new System.Drawing.Point(70, 271);
+            this.carpetButton.Name = "carpetButton";
+            this.carpetButton.Size = new System.Drawing.Size(75, 23);
+            this.carpetButton.TabIndex = 3;
+            this.carpetButton.Text = "Carpet";
+            this.carpetButton.UseVisualStyleBackColor = true;
+            this.carpetButton.Click += new System.EventHandler(this.carpetButton_Click);
+            // 
+            // saleButton
+            // 
+            this.saleButton.Location = new System.Drawing.Point(70, 360);
+            this.saleButton.Name = "saleButton";
+            this.saleButton.Size = new System.Drawing.Size(75, 23);
+            this.saleButton.TabIndex = 4;
+            this.saleButton.Text = "Bill";
+            this.saleButton.UseVisualStyleBackColor = true;
+            this.saleButton.Click += new System.EventHandler(this.saleButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saleButton);
+            this.Controls.Add(this.carpetButton);
+            this.Controls.Add(this.areaButton);
+            this.Controls.Add(this.wayneButton);
+            this.Controls.Add(this.displayLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label displayLabel;
+        private System.Windows.Forms.Button wayneButton;
+        private System.Windows.Forms.Button areaButton;
+        private System.Windows.Forms.Button carpetButton;
+        private System.Windows.Forms.Button saleButton;
+    }
+}
+
---
+++ VariablesPractice/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VariablesPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void displayLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wayneButton_Click(object sender, EventArgs e)
+        {
+            string playerName = "Wayne Gretzky";
+            int playerNumber = 99;
+            displayLabel.Text = $"{playerName} is number {playerNumber}";
+        }
+
+        private void areaButton_Click(object sender, EventArgs e)
+        {
+            double radius = 15;
+            double pi = 3.14;
+            double area = pi * (radius * radius); 
+            displayLabel.Text = $"The area of a circle with a radius of {radius} is {area}cm^2";
+        }
+
+
+
+        private void carpetButton_Click(object sender, EventArgs e)
+        {
+            //Create a double length variable that stores the values 8.5
+            double length = 8.5f;
+
+            //Create a double width variable that stores the value 6
+            double width = 6;
+
+            //Create a double costPerMeter variable that stores the value 19.95
+            double costPerMeter = 19.95;
+
+            //Create a double area variable that stores the calculated value for the area of the room
+            double area = length * width;
+
+            //Create a double totalCost variable that stores the value of the calculated total cost
+            double totalCost = area * costPerMeter;
+
+            //The area of a room with dimensions 8.5m x 6m is 51m ^ 2
+
+
+
+
+            //The cost to carpet this area at 19.95 per square metre is $1017.45
+            displayLabel.Text = $"The area of a room with the demensions {length}m x {width}m is {area} ^ 2";
+            displayLabel.Text += $"\nThe cost to carpet this area at ${costPerMeter} per square metre is {totalCost}";
+        }
+
+        private void saleButton_Click(object sender, EventArgs e)
+        {
+            //A shirt was purchased for $12.49 and was paid with a $20 bill.
+
+            //Calculate taxes, (HST = 13 %), total bill, and change.
+
+            //Display the following output, using the values in the variables: 
+
+            //Bill of Sale
+
+            //Shirt:       12.49
+
+            //Tax: 1.62
+
+            //Total: 14.11
+
+            //Tendered: 20.00
+
+            //Change: 5.89
+
+            double price = 12.49;
+            double paid = 20;
+            double taxRate = 0.13;
+            double totalPrice = price * (1 + taxRate);
+            double change = paid - totalPrice;
+
+            displayLabel.Text = $"Bill of Sale:\nShirt Price: {price}\nTax: {price * (1 + taxRate) - price}\nTotal: {totalPrice}\nTendered: {paid}\nChange: {change}";
+        }
+    }
+}
